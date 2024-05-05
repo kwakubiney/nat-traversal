e84b56701ceebef4e80c8e9cf24f346b8589141d
kwakubiney <kwaku.biney@hubtel.com>
2024-05-05 11:04:45 +0000

    feat: add peer connections to client when new peer detected
    Signed-off-by: kwakubiney <kwaku.biney@hubtel.com>

--- relaying/server.go
+++ relaying/server.go
@@ -5,62 +5,83 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strconv"
+	"net/netip"
 	"sync"
 	"time"
 )
 
 type Server struct {
-	Config          ServerConfig
-	NetworkTopology map[string][]*net.UDPAddr
-	Lock            sync.Mutex
-	Duration        time.Duration
+	Config           ServerConfig
+	NetworkTopology  map[string][]netip.AddrPort
+	Lock             sync.Mutex
+	Duration         time.Duration
+	NewMemberAlert   chan struct{}
+	ServerListenConn *net.UDPConn
 }
 
-func NewServer(config ServerConfig, duration time.Duration) Server {
+func NewServer(config ServerConfig, duration time.Duration, conn *net.UDPConn) Server {
 	return Server{
-		Config:          config,
-		NetworkTopology: map[string][]*net.UDPAddr{},
-		Duration:        duration,
+		Config:           config,
+		NetworkTopology:  map[string][]netip.AddrPort{},
+		Duration:         duration,
+		NewMemberAlert:   make(chan struct{}),
+		ServerListenConn: conn,
 	}
 }
 
-func (s *Server) Start() error {
-	localAddress, _ := strconv.Atoi(s.Config.LocalAddress)
-	serverConn, err := net.ListenUDP("udp", &net.UDPAddr{Port: localAddress})
-
-	if err != nil {
-		return fmt.Errorf("failed to listen on port : %w", err)
-	}
+func (s *Server) Start() {
+	fmt.Println("Listening for connections...")
 	for {
+		defer s.Stop()
 		buf := make([]byte, 1500)
-		fmt.Println("listening...")
-		n, addr, err := serverConn.ReadFromUDP(buf)
+		n, addr, err := s.ServerListenConn.ReadFromUDP(buf)
 		buf = buf[:n]
 		if err != nil {
 			log.Println(fmt.Errorf("error during reading udp buffer: %w", err))
 			continue
 		}
-
 		var message Heartbeat
 		err = json.Unmarshal(buf, &message)
 		if err != nil {
 			log.Println(fmt.Errorf("error during reading udp buffer: %w", err))
 			continue
 		}
-		s.addAddress(message.NetworkName, addr)
-		fmt.Println("received message from", addr)
+		var isNewMember = s.addAddress(message.NetworkName, addr)
+
+		if isNewMember {
+			resp, err := json.Marshal(s.NetworkTopology[message.NetworkName])
+			if err != nil {
+				log.Println(fmt.Errorf("error during marshalling udp buffer: %w", err))
+				continue
+			}
+			for _, lookedUpAddress := range s.NetworkTopology[message.NetworkName] {
+				if lookedUpAddress == addr.AddrPort() {
+					continue
+				}
+				_, err = s.ServerListenConn.WriteToUDPAddrPort(resp, lookedUpAddress)
+				if err != nil {
+					log.Println(fmt.Errorf("error during writing udp buffer: %w", err))
+					continue
+				}
+			}
+		}
 	}
 }
 
-func (s *Server) addAddress(networkName string, addr *net.UDPAddr) {
+func (s *Server) addAddress(networkName string, addr *net.UDPAddr) bool {
 	s.Lock.Lock()
 	defer s.Lock.Unlock()
 
 	for _, existingAddr := range s.NetworkTopology[networkName] {
-		if existingAddr.IP.Equal(addr.IP) && existingAddr.Port == addr.Port {
-			return
+		if existingAddr == addr.AddrPort() {
+			return false
 		}
 	}
-	s.NetworkTopology[networkName] = append(s.NetworkTopology[networkName], addr)
+	//Add AddrPort if we have never seen it before
+	s.NetworkTopology[networkName] = append(s.NetworkTopology[networkName], addr.AddrPort())
+	return true
+}
+
+func (s *Server) Stop() {
+	s.ServerListenConn.Close()
 }
